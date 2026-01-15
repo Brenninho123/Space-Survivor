a0e9ef07f5ff0a36b23ef4e6b9c013e1b211e51f
Ander <157061485+Brenninho123@users.noreply.github.com>
2026-01-15 13:50:02 -0300

    Atualizar o main.js

--- main.js
+++ main.js
@@ -2,10 +2,12 @@ const { app, BrowserWindow } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 900,
-    height: 700,
+    width: 1280,
+    height: 720,
+    fullscreen: true,
     resizable: false,
     title: "Space Survivor",
+    autoHideMenuBar: true,
     icon: "icon.ico",
     webPreferences: {
       contextIsolation: true
